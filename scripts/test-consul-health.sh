5e6b86defdbeafcb48245aae4c01114ffeb37b5f
dyrnq <dyrnq@qq.com>
2023-02-28 21:16:18 +0800

    Add timeout for consul check

--- scripts/test-consul-health.sh
+++ scripts/test-consul-health.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
-nerdctl rm -f nginx-1 2>/dev/null || true
-nerdctl rm -f nginx-2 2>/dev/null || true
 
+for i in 1 2 4; do
+    nerdctl rm -f nginx-$i 2>/dev/null || true
+    mkdir -p /tmp/nginx-$i && echo "nginx-$i" > /tmp/nginx-$i/index.html
+    nerdctl run -d -p "${i}"8080:80 --name nginx-$i -v /tmp/nginx-$i:/usr/share/nginx/html nginx:latest
+done
 
-mkdir -p /tmp/nginx-1 && echo "nginx-1" > /tmp/nginx-1/index.html
-mkdir -p /tmp/nginx-2 && echo "nginx-2" > /tmp/nginx-2/index.html
-
-nerdctl run -d -p 18080:80 --name nginx-1 -v /tmp/nginx-1:/usr/share/nginx/html nginx:latest
-nerdctl run -d -p 28080:80 --name nginx-2 -v /tmp/nginx-2:/usr/share/nginx/html nginx:latest
-
+echo "deregister all nginx instance"
+for i in 1 2 3 4; do
+    curl -fsS http://127.0.0.1:8500/v1/agent/service/deregister/nginx-"$i" -X PUT || true
+done
 
 echo "register an health nginx instance"
 curl http://127.0.0.1:8500/v1/agent/service/register -X PUT -d '
@@ -21,7 +22,8 @@ curl http://127.0.0.1:8500/v1/agent/service/register -X PUT -d '
     "tags": ["dev"],
     "checks": [{
         "http": "http://192.168.33.4:18080",
-        "interval": "1s"
+        "interval": "1s",
+        "timeout": "6ms"
     }]
 }
 '
@@ -35,7 +37,8 @@ curl http://127.0.0.1:8500/v1/agent/service/register -X PUT -d '
     "tags": ["dev"],
     "checks": [{
         "http": "http://192.168.33.4:28080",
-        "interval": "1s"
+        "interval": "1s",
+        "timeout": "6ms"
     }]
 }
 '
@@ -50,7 +53,8 @@ curl http://127.0.0.1:8500/v1/agent/service/register -X PUT -d '
     "tags": ["dev"],
     "checks": [{
         "http": "http://192.168.33.4:38080",
-        "interval": "1s"
+        "interval": "1s",
+        "timeout": "6ms"
     }]
 }
 '
@@ -93,3 +97,5 @@ dig @127.0.0.1 -p 8600 -t srv nginx.service.dc1.consul. +short
 # 192.168.33.4:18080
 # 192.168.33.4:28080
 # 192.168.33.4:48080
+
+# bash ./consul_check_services_changes.sh http://127.0.0.1:8500/v1/health/state/any
