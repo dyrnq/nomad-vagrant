e467f1dbded2260c1b582de1e64e14b072c7bd7b
dyrnq <dyrnq@qq.com>
2023-07-19 00:18:46 +0800

    Add nomad discovery for apisix test case

---
+++ scripts/test-apisix-discovery-nomad.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+
+id=50002
+if curl -fSL http://127.0.0.1:9180/apisix/admin/routes/$id -X GET -H 'X-API-KEY: edd1c9f034335f136f87ad84b625c8f1' | grep createdIndex; then
+    :
+else
+curl http://127.0.0.1:9180/apisix/admin/routes/$id -H 'X-API-KEY: edd1c9f034335f136f87ad84b625c8f1' -X PUT -i -d '
+{
+  "uri": "/",
+  "name": "nomad-discovery",
+  "upstream": {
+    "timeout": {
+      "connect": 6,
+      "send": 6,
+      "read": 6
+    },
+    "type": "roundrobin",
+    "scheme": "http",
+    "discovery_type": "consul",
+    "pass_host": "pass",
+    "service_name": "netshoot-2-netshoot-group",
+    "keepalive_pool": {
+      "idle_timeout": 60,
+      "requests": 1000,
+      "size": 320
+    }
+  }
+}'
+fi
+
+while true; do
+    echo -n "--->$(date)===";
+    curl --connect-timeout 1 --max-time 1 http://127.0.0.1:9080;
+    echo -n "fetch X-Consul-Index change---->"
+    curl -fsSL -i http://127.0.0.1:8500/v1/health/state/any |grep X-Consul-Index
+    echo "fetch health from consul";
+    curl -fsSL http://127.0.0.1:8500/v1/health/service/netshoot-2-netshoot-group?passing=true | jq -r '.[] | "\(.Service.Address):\(.Service.Port)"';
+    echo "dump from apisix ******" ;
+    curl -fsL http://127.0.0.1:9090/v1/discovery/consul/dump | jq -r '.services."netshoot-2-netshoot-group"[] | "\(.host):\(.port)"';
+    sleep 1s;
+done
