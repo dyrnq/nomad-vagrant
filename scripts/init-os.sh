09e90899384404653f029c19c5553d6f2656ebbb
dyrnq <dyrnq@qq.com>
2024-06-09 10:38:48 +0800

    Amend /etc/security/limits.conf

--- scripts/init-os.sh
+++ scripts/init-os.sh
@@ -128,6 +128,17 @@ cat > /etc/security/limits.conf <<'EOF'
 *       soft        memlock     32000
 *       hard        memlock     32000
 *       soft        msgqueue    8192000
+*       hard        msgqueue    8192000
+root       soft        core        unlimited
+root       hard        core        unlimited
+root       soft        nproc       1000000
+root       hard        nproc       1000000
+root       soft        nofile      1000000
+root       hard        nofile      1000000
+root       soft        memlock     32000
+root       hard        memlock     32000
+root       soft        msgqueue    8192000
+root       hard        msgqueue    8192000
 EOF
 
 
